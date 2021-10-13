35c4c258b6f732ca37cf216ed7572d70036a1b9a
amos <pitifulnoble@outlook.com>
2021-10-13 21:47:38 +0800

    添加注释

--- utils/http.js
+++ utils/http.js
@@ -1,6 +1,9 @@
 import {config} from "../config/config";
 import {promisic} from "../miniprogram_npm/lin-ui/utils/util";
 
+/**
+ * 封装请求接口，返回promise
+ */
 export class Http {
     static async request({url, data, method = 'GET'}) {
         const res = await promisic(wx.request)({
